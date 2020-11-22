362d8bd244f18e673839e4bf374fd26f58d7a1e1
Lucas Paulo <lucaspaulodev@gmail.com>
2020-11-22 00:12:57 -0300

    Dinamic Imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,12 @@ interface HomeProps {
 }
 
 export default function Home({recommendedProducts}: HomeProps) {
+  async function handleSum() {
+      const {sum} = (await import('../lib/math')).default
+
+    alert(sum(3,5));
+  }
+
   return (
     <div>
       <section>
@@ -26,6 +32,7 @@ export default function Home({recommendedProducts}: HomeProps) {
           })}
         </ul>
       </section>
+      <button onClick={handleSum}>Sum!</button>
     </div>
   )
 }
